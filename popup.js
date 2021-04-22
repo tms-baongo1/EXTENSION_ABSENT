0a9cef048a8650bf93825bd5692fe3f6530fb590
honguyen <tt.ho.nguyen@tomosia.com>
2021-04-22 15:21:17 +0700

    Init

---
+++ popup.js
@@ -0,0 +1,14 @@
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
+
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
